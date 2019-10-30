e93575423ecdc1730d7ca5f3907697167163d1bc
Mikita_Charniauski <ccharniauski@travix.com>
2019-10-30 18:13:51 +0300

    clean updated code

--- IisHostService/IisHostService/Program.cs
+++ IisHostService/IisHostService/Program.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace IisHostService
 {
@@ -14,26 +7,15 @@ namespace IisHostService
     {
         public static void Main(string[] args)
         {
-            //CreateHostBuilder(args).Build().Run();
-            CreateHostBuilder(args);
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static void CreateHostBuilder(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseUrls("http://*:5000/")
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseStartup<Startup>()
-                .Build();
-            host.Run();
-        }
-
-        //public static IHostBuilder CreateHostBuilder(string[] args) =>
-        //    Host.CreateDefaultBuilder(args)
-        //        .ConfigureWebHostDefaults(webBuilder =>
-        //        {
-        //            webBuilder.UseStartup<Startup>();
-        //        });
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                    webBuilder
+                        .UseStartup<Startup>()
+                        .CaptureStartupErrors(true)
+                );
     }
 }
